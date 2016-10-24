553f83a3efb22fb92a8903efe77e657e3999df80
hailinglin <hailing.lin@modularrnd.com>
2016-10-24 12:21:43 +0800

    add theme file

--- Apps/Admin/Conf/config.php
+++ Apps/Admin/Conf/config.php
@@ -1,4 +1,11 @@
 <?php
 return array(
 	//'配置项'=>'配置值'
+	'TMPL_PARSE_STRING'  =>array(
+		'__JS__'  => '/Public/JS/', // 增加新的JS类库路径替换规则
+		'__ACSS__' => 'Public/admin/css/', // 增加新的admin css类库路径替换规则
+		'__AJS__' => 'Public/admin/js/', // 增加新的admin js类库路径替换规则
+		'__AIMG__' => 'Public/admin/images/', // 增加新的admin images类库路径替换规则
+		'__UPLOAD__' => '/Uploads', // 增加新的上传路径替换规则
+	)
 );
--- Apps/Admin/Controller/IndexController.class.php
+++ Apps/Admin/Controller/IndexController.class.php
@@ -3,6 +3,8 @@ namespace Admin\Controller;
 use Think\Controller;
 class IndexController extends Controller {
     public function index(){
-        $this->show('admin');
+        // $this->show('admin');
+		$this->assign('title',"测试标题");
+		$this->display();
     }
 }
