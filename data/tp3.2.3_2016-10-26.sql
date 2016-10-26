d00296e0336c245af82987084ef8341fc7ac7235
hailinglin <hailing.lin@modularrnd.com>
2016-10-26 18:24:50 +0800

    update database....

---
+++ data/tp3.2.3_2016-10-26.sql
@@ -0,0 +1,174 @@
+# ************************************************************
+# Sequel Pro SQL dump
+# Version 4499
+#
+# http://www.sequelpro.com/
+# https://github.com/sequelpro/sequelpro
+#
+# Host: 127.0.0.1 (MySQL 5.6.27)
+# Database: tp3.2.3
+# Generation Time: 2016-10-26 10:21:12 +0000
+# ************************************************************
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+# Dump of table tp_access
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `tp_access`;
+
+CREATE TABLE `tp_access` (
+  `role_id` smallint(6) unsigned NOT NULL,
+  `node_id` smallint(6) unsigned NOT NULL,
+  `level` tinyint(1) NOT NULL,
+  `module` varchar(50) DEFAULT NULL,
+  KEY `groupId` (`role_id`),
+  KEY `nodeId` (`node_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+LOCK TABLES `tp_access` WRITE;
+/*!40000 ALTER TABLE `tp_access` DISABLE KEYS */;
+
+INSERT INTO `tp_access` (`role_id`, `node_id`, `level`, `module`)
+VALUES
+	(2,4,2,NULL),
+	(1,5,3,NULL),
+	(1,3,2,NULL);
+
+/*!40000 ALTER TABLE `tp_access` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+# Dump of table tp_admin_user
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `tp_admin_user`;
+
+CREATE TABLE `tp_admin_user` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT 'UID',
+  `user_type` int(11) NOT NULL DEFAULT '1' COMMENT '用户类型',
+  `nickname` varchar(63) DEFAULT NULL COMMENT '昵称',
+  `username` varchar(31) NOT NULL DEFAULT '' COMMENT '用户名',
+  `password` varchar(63) NOT NULL DEFAULT '' COMMENT '密码',
+  `email` varchar(63) NOT NULL DEFAULT '' COMMENT '邮箱',
+  `email_bind` tinyint(1) NOT NULL DEFAULT '0' COMMENT '邮箱验证',
+  `mobile` varchar(11) NOT NULL DEFAULT '' COMMENT '手机号',
+  `mobile_bind` tinyint(1) NOT NULL DEFAULT '0' COMMENT '邮箱验证',
+  `avatar` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '头像',
+  `score` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '积分',
+  `money` decimal(11,2) NOT NULL DEFAULT '0.00' COMMENT '余额',
+  `reg_ip` bigint(20) NOT NULL DEFAULT '0' COMMENT '注册IP',
+  `reg_type` varchar(15) NOT NULL DEFAULT '' COMMENT '注册方式',
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '更新时间',
+  `status` tinyint(3) NOT NULL DEFAULT '0' COMMENT '状态',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户账号表';
+
+LOCK TABLES `tp_admin_user` WRITE;
+/*!40000 ALTER TABLE `tp_admin_user` DISABLE KEYS */;
+
+INSERT INTO `tp_admin_user` (`id`, `user_type`, `nickname`, `username`, `password`, `email`, `email_bind`, `mobile`, `mobile_bind`, `avatar`, `score`, `money`, `reg_ip`, `reg_type`, `create_time`, `update_time`, `status`)
+VALUES
+	(3,1,'henry','admin','1690542b89d7391a795c0a6aa64a5459','',0,'',0,0,0,0.00,0,'','2016-10-24 13:54:16','2016-10-24 13:54:16',1),
+	(4,1,'henry','henry','027e4180beedb29744413a7ea6b84a42','lin_hailing@sina.com',1,'18721186620',1,0,0,0.00,0,'1','2016-10-26 17:34:14','2016-10-26 17:34:14',0);
+
+/*!40000 ALTER TABLE `tp_admin_user` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+# Dump of table tp_node
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `tp_node`;
+
+CREATE TABLE `tp_node` (
+  `id` smallint(6) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) NOT NULL,
+  `title` varchar(50) DEFAULT NULL,
+  `status` tinyint(1) DEFAULT '0',
+  `remark` varchar(255) DEFAULT NULL,
+  `sort` smallint(6) unsigned DEFAULT NULL,
+  `pid` smallint(6) unsigned NOT NULL,
+  `level` tinyint(1) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `level` (`level`),
+  KEY `pid` (`pid`),
+  KEY `status` (`status`),
+  KEY `name` (`name`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+LOCK TABLES `tp_node` WRITE;
+/*!40000 ALTER TABLE `tp_node` DISABLE KEYS */;
+
+INSERT INTO `tp_node` (`id`, `name`, `title`, `status`, `remark`, `sort`, `pid`, `level`)
+VALUES
+	(1,'Admin','后台权限管理',1,NULL,1,0,1),
+	(2,'Home','前段权限管理',1,NULL,1,0,1),
+	(3,'Config','后台配置',1,NULL,1,1,2),
+	(4,'Nav','导航管理',1,NULL,1,1,2),
+	(5,'group','参数配置',1,NULL,1,3,3),
+	(6,'Rbac','Rbac管理',1,NULL,1,1,2);
+
+/*!40000 ALTER TABLE `tp_node` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+# Dump of table tp_role
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `tp_role`;
+
+CREATE TABLE `tp_role` (
+  `id` smallint(6) unsigned NOT NULL AUTO_INCREMENT,
+  `pid` smallint(6) DEFAULT NULL,
+  `status` tinyint(1) unsigned DEFAULT NULL,
+  `name` varchar(20) NOT NULL,
+  `remark` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `pid` (`pid`),
+  KEY `status` (`status`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+LOCK TABLES `tp_role` WRITE;
+/*!40000 ALTER TABLE `tp_role` DISABLE KEYS */;
+
+INSERT INTO `tp_role` (`id`, `pid`, `status`, `name`, `remark`)
+VALUES
+	(1,0,1,'管理员','普通管理员'),
+	(2,0,1,'编辑','新闻编辑者');
+
+/*!40000 ALTER TABLE `tp_role` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+# Dump of table tp_role_user
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `tp_role_user`;
+
+CREATE TABLE `tp_role_user` (
+  `role_id` mediumint(9) unsigned DEFAULT NULL,
+  `user_id` char(32) DEFAULT NULL,
+  KEY `group_id` (`role_id`),
+  KEY `user_id` (`user_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
